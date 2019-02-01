b7ff2da0377516dc0ee6dda4370ccda57e1b7b16
macjabeth <macbethjonathan@gmail.com>
2019-02-01 17:27:26 +0000

    complete: carousel functionality

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,14 +1,47 @@
 class Carousel {
+  constructor (element) {
+    // assign this.element to the carousel
+    this.element = element;
 
+    // grab the laft and right buttons
+    this.leftBtn = this.element.firstElementChild;
+    this.rightBtn = this.element.lastElementChild;
+
+    // grab a reference to all of the images
+    this.images = this.element.querySelectorAll('img');
+
+    // create a current index
+    this.index = 0;
+
+    // display the current image
+    this.images[this.index].style.display = 'block';
+
+    // add click handlers to the buttons
+    this.leftBtn.addEventListener('click', () => this.slideLeft());
+    this.rightBtn.addEventListener('click', () => this.slideRight());
+  }
+
+  slideLeft () {
+    // hide the current image
+    this.images[this.index].style.display = 'none';
+
+    // cycle the index
+    this.index = this.index === 0 ? this.images.length - 1 : --this.index;
+
+    // display the new image
+    this.images[this.index].style.display = 'block';
+  }
+
+  slideRight () {
+    // hide the current image
+    this.images[this.index].style.display = 'none';
+
+    // cycle the index
+    this.index = this.index === this.images.length - 1 ? 0 : ++this.index;
+
+    // display the new image
+    this.images[this.index].style.display = 'block';
+  }
 }
 
-let carousel = document.querySelector();
-
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
+const carousel = new Carousel(document.querySelector('.carousel'));
