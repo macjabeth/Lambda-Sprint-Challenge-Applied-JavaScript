6604373c4cfd63c1e635a0a42ef0a369aa9b5cb1
macjabeth <macbethjonathan@gmail.com>
2019-02-02 01:21:41 +0000

    update: carousel slide in transition effect

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -10,44 +10,47 @@ class Carousel {
     // grab a reference to all of the images
     this.images = this.element.querySelectorAll('img');
 
+    // set the first image to be visible
+    this.images[0].style.visibility = 'visible';
+
     // create a current index
     this.index = 0;
-
-    // display the current image
-    this.images[this.index].style.visibility = 'visible';
-    this.images[this.index].style.opacity = 1;
 
     // add click handlers to the buttons
     this.leftBtn.addEventListener('click', () => this.slideLeft());
     this.rightBtn.addEventListener('click', () => this.slideRight());
   }
 
+  slideRight () {
+    // store the previous index
+    const previousIndex = this.index;
+
+    // cycle the current index
+    this.index = this.index === this.images.length - 1 ? 0 : ++this.index;
+
+    // set the next image to slide in from the right
+    this.images[this.index].style.animation = '0.6s rightslidein';
+    this.images[this.index].style.animationFillMode = 'forwards';
+
+    // slide the current image out to the left
+    this.images[previousIndex].style.animation = '0.6s rightslideout';
+    this.images[previousIndex].style.animationFillMode = 'forwards';
+  }
+
   slideLeft () {
-    // hide the current image
-    this.images[this.index].style.visibility = 'hidden';
-    this.images[this.index].style.opacity = 0;
+    // store the previous index
+    const previousIndex = this.index;
 
     // cycle the index
     this.index = this.index === 0 ? this.images.length - 1 : --this.index;
 
-    // display the new image
-    this.images[this.index].style.visibility = 'visible';
-    this.images[this.index].style.opacity = 1;
-  }
+    // set the next image to slide in from the left
+    this.images[this.index].style.animation = '0.6s leftslidein';
+    this.images[this.index].style.animationFillMode = 'forwards';
 
-  slideRight () {
-    // hide the current image
-    this.images[this.index].style.visibility = 'hidden';
-    this.images[this.index].style.opacity = 0;
-
-    // cycle the index
-    this.index = this.index === this.images.length - 1 ? 0 : ++this.index;
-
-    // display the new image - leaving the setTimeout here to see the transition differences between the two buttons
-    setTimeout(() => {
-      this.images[this.index].style.visibility = 'visible';
-      this.images[this.index].style.opacity = 1;
-    }, 300)
+    // slide the current image out to the left
+    this.images[previousIndex].style.animation = '0.6s leftslideout';
+    this.images[previousIndex].style.animationFillMode = 'forwards';
   }
 }
 
