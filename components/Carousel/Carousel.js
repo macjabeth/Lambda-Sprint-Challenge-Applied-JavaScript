51910190ea00af0174ea0c234cb09b106621b8be
macjabeth <macbethjonathan@gmail.com>
2020-05-08 13:54:31 -0400

    create carousel component

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,56 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const imagePaths = [
+  './assets/carousel/mountains.jpeg',
+  './assets/carousel/computer.jpeg',
+  './assets/carousel/trees.jpeg',
+  './assets/carousel/turntable.jpeg'
+];
+
+function Carousel(imagePaths) {
+  // create elements
+  const carousel = document.createElement('div');
+  const leftBtn = document.createElement('div');
+  const rightBtn = document.createElement('div');
+  const images = imagePaths.map(imagePath => {
+    const img = document.createElement('img');
+    img.src = imagePath;
+    return img;
+  });
+
+  // create structure
+  carousel.append(leftBtn, ...images, rightBtn);
+
+  // add attributes and data
+  carousel.classList.add('carousel');
+  leftBtn.classList.add('left-button');
+  leftBtn.textContent = '<';
+  rightBtn.classList.add('right-button');
+  rightBtn.textContent = '>';
+
+  // handle functionality
+  let index = 0;
+
+  rightBtn.addEventListener('click', () => {
+    images[index].style.display = 'none';
+    if (++index === images.length) index = 0;
+    images[index].style.display = 'block';
+  });
+
+  leftBtn.addEventListener('click', () => {
+    images[index].style.display = 'none';
+    if (--index === -1) index = images.length - 1;
+    images[index].style.display = 'block';
+  });
+
+  images[index].style.display = 'block';
+
+  // return carousel component
+  return carousel;
+}
+
+const carouselContainer = document.querySelector('.carousel-container');
+
+carouselContainer.append(Carousel(imagePaths));
